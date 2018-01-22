5dbd09c5b7663f805b6f79c9a0e956fe8ad7880c
Dylan <dylan@aukibit.com.au>
2018-01-22 20:56:53 +1000

    BLE and WiFi together in the ino file, some refactoring, can't run WiFi and BLE together currently

--- esp32/node/ConnContainer.cpp
+++ esp32/node/ConnContainer.cpp
@@ -1,8 +1,19 @@
+#include <iomanip>
 #include <sstream>
 #include <string>
 
 #include "ConnContainer.hpp"
-#include "helper_functions.hpp"
+
+std::string int_to_mac(const uint64_t* mem) {
+	std::stringstream stream;
+	stream << std::setfill('0') << std::setw(2) << std::hex << (int)((uint8_t *)(mem))[0] << ':';
+	stream << std::setfill('0') << std::setw(2) << std::hex << (int)((uint8_t *)(mem))[1] << ':';
+	stream << std::setfill('0') << std::setw(2) << std::hex << (int)((uint8_t *)(mem))[2] << ':';
+	stream << std::setfill('0') << std::setw(2) << std::hex << (int)((uint8_t *)(mem))[3] << ':';
+	stream << std::setfill('0') << std::setw(2) << std::hex << (int)((uint8_t *)(mem))[4] << ':';
+	stream << std::setfill('0') << std::setw(2) << std::hex << (int)((uint8_t *)(mem))[5];
+	return stream.str();
+}
 
 ConnContainer::ConnContainer(uint64_t chipid, std::string addr, int rssi) :
     esp32_id(chipid), beacon_addr(addr), rssi(rssi) {}
--- esp32/node/node.ino
+++ esp32/node/node.ino
@@ -6,18 +6,50 @@
 #include <BLEUtils.h>
 #include <BLEScan.h>
 #include <BLEAdvertisedDevice.h>
+#include <WiFi.h>
 
 // Local includes
 #include "ConnContainer.hpp"
-#include "helper_functions.hpp"
-#include "LocalWifiServer.hpp"
 
-// Global variables
+// BLE global variables
 int scan_time = 5; // seconds
 BLEScan* pBLEScan;
 BLEScanResults found_devices;
-std::string ssid = "";
-std::string wifi_password = "auk!b1t";
+
+// WiFi global variables
+const char* ssid = "TelstraADBF11";
+const char* password = "fvpj9y84fm";
+WiFiServer server(80);
+WiFiClient client;
+std::string ln;
+char c;
+
+// Local functions
+void serve_wifi_client(void) {
+    client = server.available();
+    if (client) {
+        ln = "";
+        while (client.connected()) {
+            if (client.available()) {
+                c = client.read();
+                if (c == '\n') {
+                    if (ln.length() == 0) {
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println();
+                        client.print("Success!<br>");
+                        client.println();
+                        break;
+                    } else {
+                        ln = "";
+                    }
+                } else if (c != '\r') {
+                    ln += c;
+                }
+            }
+        }
+    }
+}
 
 class AdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
@@ -30,16 +62,32 @@ class AdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     }
 };
 
+// Arduino functions
 void setup() {
     Serial.begin(115200);
 
+    // BLE setup
     BLEDevice::init("");
     pBLEScan = BLEDevice::getScan();
     pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
     pBLEScan->setActiveScan(true); // More power, but faster
+
+    // WiFi setup
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(50);
+    }
+    server.begin();
+    
 }
 
 void loop() {
     Serial.printf("Starting a scan for %d seconds...\n\r", scan_time);
+    
+    /* Can't do both of these one after the other...? */
+    // BLE
     found_devices = pBLEScan->start(scan_time);
+
+    // WiFi   
+    serve_wifi_client();
 }  
