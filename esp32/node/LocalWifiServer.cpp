7bebf380fdae59932bb1753dc74234176dfd284d
Dylan <dylan@aukibit.com.au>
2018-01-22 21:00:06 +1000

    VSCode changes commit

--- esp32/node/LocalWifiServer.cpp
+++
@@ -1,6 +0,0 @@
-#include "LocalWifiServer.hpp"
-
-LocalWifiServer::LocalWifiServer(std::string ssid, std::string password) {
-    WiFi.begin(ssid.c_str(), password.c_str());
-    this->server.begin();
-}
--- esp32/node/helper_functions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef HELPER_FUNCTIONS_NODE_H
-#define HELPER_FUNCTIONS_NODE_H
-
-#include <string>
-#include <cstdint>
-
-std::string int_to_mac(const uint64_t* mem);    
-
-#endif
