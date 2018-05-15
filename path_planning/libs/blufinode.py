c2456976133c79682bdc80cfe6ab5e376c2643de
Dylan <dylan@aukibit.com.au>
2018-05-15 19:32:23 +1000

    refactoring

--- path_planning/libs/blufinode.py
+++
@@ -1,8 +0,0 @@
-class BlufiNode:
-    """ A class describing an Aukibit BluFi node """
-    def __init__(self, ip, mac):
-        self.ip = ip
-        self.mac = mac
-
-    def __repr__(self):
-        return "MAC: " + self.mac + ", IP: " + self.ip
--- path_planning/libs/packetrcv.py
+++
@@ -1,9 +0,0 @@
-class PacketRcv:
-    """ A class containing information on a packet read event. """
-    def __init__(self, node, timestamp, rssi):
-        self.node = node
-        self.timestamp = timestamp
-        self.rssi = rssi
-
-    def __repr__(self):
-        return self.node.__repr__() + ", RSSI: " + self.rssi + ", Timestamp: " + self.timestamp
